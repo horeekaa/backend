73325a73693153b766d472c8089cc757c3d1dae6
stanley97dion <stanley97dion@gmail.com>
2021-01-19 11:32:04 +0700

    person repo and schema

---
+++ repositories/mongoRepos/person.go
@@ -0,0 +1,142 @@
+package mongorepos
+
+import (
+	model "github.com/horeekaa/backend/model"
+	databaseclient "github.com/horeekaa/backend/repositories/databaseClient"
+	mongooperations "github.com/horeekaa/backend/repositories/databaseClient/operations"
+)
+
+type PersonRepoMongo struct {
+	basicOperation *mongooperations.BasicOperation
+}
+
+func NewPersonRepoMongo(mongoRepo *databaseclient.MongoRepository) *PersonRepoMongo {
+	return &PersonRepoMongo{
+		basicOperation: &mongooperations.BasicOperation{
+			Client:         (*mongoRepo).Client,
+			CollectionRef:  (*mongoRepo.Client.Database((*mongoRepo).DatabaseName)).Collection("persons"),
+			Timeout:        (*mongoRepo).Timeout,
+			CollectionName: "persons",
+		},
+	}
+}
+
+func (prsnRepoMongo *PersonRepoMongo) FindByID(id string, operationOptions *mongooperations.OperationOptions) (*model.Person, error) {
+	object, err := prsnRepoMongo.basicOperation.FindByID(id, operationOptions)
+
+	return ((*object).(*model.Person)), err
+}
+
+func (prsnRepoMongo *PersonRepoMongo) FindOne(query map[string]interface{}, operationOptions *mongooperations.OperationOptions) (*model.Person, error) {
+	object, err := prsnRepoMongo.basicOperation.FindOne(query, operationOptions)
+
+	return ((*object).(*model.Person)), err
+}
+
+func (prsnRepoMongo *PersonRepoMongo) Find(query map[string]interface{}, operationOptions *mongooperations.OperationOptions) ([]*model.Person, error) {
+	objects, err := prsnRepoMongo.basicOperation.Find(query, operationOptions)
+
+	var persons = []*model.Person{}
+	for _, obj := range objects {
+		persons = append(persons, (*obj).(*model.Person))
+	}
+
+	return persons, err
+}
+
+type personCreateOutput struct {
+	ID     string
+	Object model.Person
+}
+
+func (prsnRepoMongo *PersonRepoMongo) Create(input *model.CreateAccount, operationOptions *mongooperations.OperationOptions) (*model.Person, error) {
+	defaultedInput, err := prsnRepoMongo.setDefaultValues(*input,
+		&defaultValuesOptions{DefaultValuesType: DefaultValuesCreateType},
+		operationOptions,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	object, err := prsnRepoMongo.basicOperation.Create(*defaultedInput, operationOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	createOutputObject := (*object).(*personCreateOutput)
+
+	person := &model.Person{
+		ID:                          createOutputObject.ID,
+		FirstName:                   createOutputObject.Object.FirstName,
+		LastName:                    createOutputObject.Object.LastName,
+		Gender:                      createOutputObject.Object.Gender,
+		PhoneNumber:                 createOutputObject.Object.PhoneNumber,
+		Email:                       createOutputObject.Object.Email,
+		NoOfRecentTransactionToKeep: createOutputObject.Object.NoOfRecentTransactionToKeep,
+	}
+
+	return person, err
+}
+
+func (prsnRepoMongo *PersonRepoMongo) Update(ID string, updateData *model.UpdateAccount, operationOptions *mongooperations.OperationOptions) (*model.Person, error) {
+	defaultedInput, err := prsnRepoMongo.setDefaultValues(*updateData,
+		&defaultValuesOptions{DefaultValuesType: DefaultValuesUpdateType},
+		operationOptions,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	object, err := prsnRepoMongo.basicOperation.Update(ID, *defaultedInput, operationOptions)
+
+	return ((*object).(*model.Person)), err
+}
+
+type setPersonDefaultValuesOutput struct {
+	CreatePerson *model.CreatePerson
+	UpdatePerson *model.UpdatePerson
+}
+
+func (prsnRepoMongo *PersonRepoMongo) setDefaultValues(input interface{}, options *defaultValuesOptions, operationOptions *mongooperations.OperationOptions) (*setPersonDefaultValuesOutput, error) {
+	var noOfRecentTransactionToKeep int
+
+	updateInput := input.(model.UpdatePerson)
+	if (*options).DefaultValuesType == DefaultValuesUpdateType {
+		existingObject, err := prsnRepoMongo.FindByID(*updateInput.ID, operationOptions)
+		if err != nil {
+			return nil, err
+		}
+
+		if &(*existingObject).NoOfRecentTransactionToKeep == nil {
+			noOfRecentTransactionToKeep = 15
+		}
+
+		return &setPersonDefaultValuesOutput{
+			UpdatePerson: &model.UpdatePerson{
+				ID:                          updateInput.ID,
+				FirstName:                   updateInput.FirstName,
+				LastName:                    updateInput.LastName,
+				Gender:                      updateInput.Gender,
+				PhoneNumber:                 updateInput.PhoneNumber,
+				Email:                       updateInput.Email,
+				NoOfRecentTransactionToKeep: &noOfRecentTransactionToKeep,
+			},
+		}, nil
+	}
+	createInput := (input).(model.CreatePerson)
+
+	if &createInput.NoOfRecentTransactionToKeep == nil {
+		noOfRecentTransactionToKeep = 15
+	}
+
+	return &setPersonDefaultValuesOutput{
+		CreatePerson: &model.CreatePerson{
+			FirstName:                   createInput.FirstName,
+			LastName:                    createInput.LastName,
+			Gender:                      createInput.Gender,
+			PhoneNumber:                 createInput.PhoneNumber,
+			Email:                       createInput.Email,
+			NoOfRecentTransactionToKeep: &noOfRecentTransactionToKeep,
+		},
+	}, nil
+}
