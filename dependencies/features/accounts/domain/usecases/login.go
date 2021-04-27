a22357728a3bf27855921fbcc8b8eed1db473801
stanley97dion <stanley97dion@gmail.com>
2021-04-27 10:57:09 +0700

    updating account dependency mgmt

--- dependencies/features/accounts/domain/usecases/login.go
+++ dependencies/features/accounts/domain/usecases/login.go
@@ -1,6 +1,7 @@
 package accountdomainusecasedependencies
 
 import (
+	"github.com/golobby/container/v2"
 	accountdomainrepositoryinterfaces "github.com/horeekaa/backend/features/accounts/domain/repositories"
 	accountpresentationusecases "github.com/horeekaa/backend/features/accounts/domain/usecases"
 	accountpresentationusecaseinterfaces "github.com/horeekaa/backend/features/accounts/presentation/usecases"
@@ -8,13 +9,17 @@ import (
 
 type LoginUsecaseDependency struct{}
 
-func (loginUsecaseDpdcy *LoginUsecaseDependency) bind(
-	manageAccountAuthenticationRepository accountdomainrepositoryinterfaces.ManageAccountAuthenticationRepository,
-	manageAccountDeviceTokenRepository accountdomainrepositoryinterfaces.ManageAccountDeviceTokenRepository,
-) accountpresentationusecaseinterfaces.LoginUsecase {
-	loginUsecase, _ := accountpresentationusecases.NewLoginUsecase(
-		manageAccountAuthenticationRepository,
-		manageAccountDeviceTokenRepository,
+func (loginUsecaseDpdcy *LoginUsecaseDependency) bind() {
+	container.Singleton(
+		func(
+			manageAccountAuthenticationRepository accountdomainrepositoryinterfaces.ManageAccountAuthenticationRepository,
+			manageAccountDeviceTokenRepository accountdomainrepositoryinterfaces.ManageAccountDeviceTokenRepository,
+		) accountpresentationusecaseinterfaces.LoginUsecase {
+			loginUsecase, _ := accountpresentationusecases.NewLoginUsecase(
+				manageAccountAuthenticationRepository,
+				manageAccountDeviceTokenRepository,
+			)
+			return loginUsecase
+		},
 	)
-	return loginUsecase
 }
