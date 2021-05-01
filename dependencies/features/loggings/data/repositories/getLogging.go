b2f322f4f6bcf5a604a5855f3b1633fff4569bf7
stanley97dion <stanley97dion@gmail.com>
2021-05-01 15:33:51 +0700

    add logging repository

---
+++ dependencies/features/loggings/data/repositories/getLogging.go
@@ -0,0 +1,23 @@
+package loggingdomainrepositorydependencies
+
+import (
+	"github.com/golobby/container/v2"
+	databaseloggingdatasourceinterfaces "github.com/horeekaa/backend/features/loggings/data/dataSources/databases/interfaces"
+	loggingdomainrepositories "github.com/horeekaa/backend/features/loggings/data/repositories"
+	loggingdomainrepositoryinterfaces "github.com/horeekaa/backend/features/loggings/domain/repositories"
+)
+
+type GetLoggingDependency struct{}
+
+func (_ *GetLoggingDependency) bind() {
+	container.Singleton(
+		func(
+			loggingDataSource databaseloggingdatasourceinterfaces.LoggingDataSource,
+		) loggingdomainrepositoryinterfaces.GetLoggingRepository {
+			getLoggingRepo, _ := loggingdomainrepositories.NewGetLoggingRepository(
+				loggingDataSource,
+			)
+			return getLoggingRepo
+		},
+	)
+}
