34ffe30e12c144c4027980a4fe0babf091bc8815
stanley97dion <stanley97dion@gmail.com>
2021-05-03 11:32:17 +0700

    memberaccessref resolve import conflict

--- dependencies/features/memberAccessRefs/data/repositories/getAllMemberAccessRef.go
+++ dependencies/features/memberAccessRefs/data/repositories/getAllMemberAccessRef.go
@@ -2,9 +2,9 @@ package memberaccessrefdomainrepositorydependencies
 
 import (
 	"github.com/golobby/container/v2"
+	databasememberaccessrefdatasourceinterfaces "github.com/horeekaa/backend/features/memberAccessRefs/data/dataSources/databases/interfaces/sources"
 	memberaccessrefdomainrepositories "github.com/horeekaa/backend/features/memberAccessRefs/data/repositories"
 	memberaccessrefdomainrepositoryinterfaces "github.com/horeekaa/backend/features/memberAccessRefs/domain/repositories"
-	databasememberaccessrefdatasourceinterfaces "github.com/horeekaa/backend/features/memberaccessrefs/data/dataSources/databases/interfaces/sources"
 )
 
 type GetAllMemberAccessRefDependency struct{}
