38991f01c5589eef0855bde6a06966e621e2e3ba
stanley97dion <stanley97dion@gmail.com>
2021-06-03 22:27:02 +0700

    Prevent creating duplicate access

--- core/errors/errors/enums/index.go
+++ core/errors/errors/enums/index.go
@@ -3,4 +3,6 @@ package horeekaacoreerrorenums
 // string enums for usecase error
 const (
 	AuthenticationTokenNotExist string = "AUTHENTICATION_TOKEN_NOT_EXIST"
+
+	DuplicateAccessExist string = "DUPLICATE_ACCESS_EXIST"
 )
