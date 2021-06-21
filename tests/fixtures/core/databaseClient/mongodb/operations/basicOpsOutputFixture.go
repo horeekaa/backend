e1a49dcac5ae6c815ba4292c56dbbcf70552b965
stanley97dion <stanley97dion@gmail.com>
2021-06-21 15:37:35 +0700

    time field removed dynamic time

--- tests/fixtures/core/databaseClient/mongodb/operations/basicOpsOutputFixture.go
+++ tests/fixtures/core/databaseClient/mongodb/operations/basicOpsOutputFixture.go
@@ -7,11 +7,15 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+var layout = "2006-01-02T15:04:05.000Z"
+var str = "2014-11-12T11:45:26.371Z"
+var t, err = time.Parse(layout, str)
+
 var BasicOpsSingleResultOutput model.Account = model.Account{
 	ID:        primitive.ObjectID{byte(255)},
 	Email:     "accountone@test.com",
 	Status:    "ACTIVE",
 	Type:      "PERSON",
-	CreatedAt: time.Now(),
+	CreatedAt: t,
 	Person:    &model.Person{ID: primitive.ObjectID{byte(200)}},
 }
