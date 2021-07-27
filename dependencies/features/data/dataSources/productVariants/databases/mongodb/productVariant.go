57c332b833f81d105ff7b2992c5b8784235080dc
stanley97dion <stanley97dion@gmail.com>
2021-07-27 23:16:34 +0700

    fix productVariant dtsource dependency import path

--- dependencies/features/data/dataSources/productVariants/databases/mongodb/productVariant.go
+++ dependencies/features/data/dataSources/productVariants/databases/mongodb/productVariant.go
@@ -3,10 +3,10 @@ package mongodbproductvariantdatasourcedependencies
 import (
 	"github.com/golobby/container/v2"
 	mongodbcoreoperationinterfaces "github.com/horeekaa/backend/core/databaseClient/mongodb/interfaces/operations"
+	databaseproductvariantdatasourceinterfaces "github.com/horeekaa/backend/features/productVariants/data/dataSources/databases/interfaces/sources"
 	mongodbproductvariantdatasources "github.com/horeekaa/backend/features/productVariants/data/dataSources/databases/mongodb"
 	mongodbproductvariantdatasourceinterfaces "github.com/horeekaa/backend/features/productVariants/data/dataSources/databases/mongodb/interfaces"
 	databaseproductvariantdatasources "github.com/horeekaa/backend/features/productVariants/data/dataSources/databases/sources"
-	databaseproductvariantdatasourceinterfaces "github.com/horeekaa/backend/features/productvariants/data/dataSources/databases/interfaces/sources"
 )
 
 type ProductVariantDataSourceDependency struct{}
