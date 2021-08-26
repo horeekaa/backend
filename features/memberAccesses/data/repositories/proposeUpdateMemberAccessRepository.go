3c172e23878772e9016cd79262b5d8b08c973ecf
stanley97dion <stanley97dion@gmail.com>
2021-08-26 18:14:50 +0700

    memberAccess repos added notification component

--- features/memberAccesses/data/repositories/proposeUpdateMemberAccessRepository.go
+++ features/memberAccesses/data/repositories/proposeUpdateMemberAccessRepository.go
@@ -1,22 +1,29 @@
 package memberaccessdomainrepositories
 
 import (
+	"encoding/json"
+
 	mongodbcoretransactioninterfaces "github.com/horeekaa/backend/core/databaseClient/mongodb/interfaces/transaction"
 	mongodbcoretypes "github.com/horeekaa/backend/core/databaseClient/mongodb/types"
 	memberaccessdomainrepositoryinterfaces "github.com/horeekaa/backend/features/memberAccesses/domain/repositories"
+	notificationdomainrepositoryinterfaces "github.com/horeekaa/backend/features/notifications/domain/repositories"
 	"github.com/horeekaa/backend/model"
+	"github.com/thoas/go-funk"
 )
 
 type proposeUpdateMemberAccessRepository struct {
+	createNotifComponent                          notificationdomainrepositoryinterfaces.CreateNotificationTransactionComponent
 	proposeUpdateMemberAccessTransactionComponent memberaccessdomainrepositoryinterfaces.ProposeUpdateMemberAccessTransactionComponent
 	mongoDBTransaction                            mongodbcoretransactioninterfaces.MongoRepoTransaction
 }
 
 func NewProposeUpdateMemberAccessRepository(
+	createNotifComponent notificationdomainrepositoryinterfaces.CreateNotificationTransactionComponent,
 	proposeUpdateMemberAccessRepositoryTransactionComponent memberaccessdomainrepositoryinterfaces.ProposeUpdateMemberAccessTransactionComponent,
 	mongoDBTransaction mongodbcoretransactioninterfaces.MongoRepoTransaction,
 ) (memberaccessdomainrepositoryinterfaces.ProposeUpdateMemberAccessRepository, error) {
 	proposeUpdateMemberAccessRepo := &proposeUpdateMemberAccessRepository{
+		createNotifComponent,
 		proposeUpdateMemberAccessRepositoryTransactionComponent,
 		mongoDBTransaction,
 	}
@@ -48,15 +55,51 @@ func (updateOrgRepo *proposeUpdateMemberAccessRepository) TransactionBody(
 	operationOption *mongodbcoretypes.OperationOptions,
 	input interface{},
 ) (interface{}, error) {
-	return updateOrgRepo.proposeUpdateMemberAccessTransactionComponent.TransactionBody(
+	memberAccessToUpdate := input.(*model.InternalUpdateMemberAccess)
+	updatedMemberAccess, err := updateOrgRepo.proposeUpdateMemberAccessTransactionComponent.TransactionBody(
 		operationOption,
-		input.(*model.InternalUpdateMemberAccess),
+		memberAccessToUpdate,
 	)
+	if err != nil {
+		return nil, err
+	}
+
+	if updatedMemberAccess.MemberAccessRefType == model.MemberAccessRefTypeOrganizationsBased &&
+		updatedMemberAccess.Account.ID.Hex() == memberAccessToUpdate.SubmittingAccount.ID.Hex() &&
+		updatedMemberAccess.ProposalStatus == model.EntityProposalStatusApproved &&
+		(funk.GetOrElse(
+			memberAccessToUpdate.InvitationAccepted,
+			false,
+		)).(bool) {
+		notificationToCreate := &model.InternalCreateNotification{
+			NotificationCategory: model.NotificationCategoryOrgInvitationAccepted,
+			PayloadOptions: &model.PayloadOptionsInput{
+				InvitationAcceptedPayload: &model.InvitationAcceptedPayloadInput{
+					MemberAccess: &model.MemberAccessForNotifPayloadInput{},
+				},
+			},
+		}
+
+		jsonTemp, _ := json.Marshal(updatedMemberAccess)
+		json.Unmarshal(jsonTemp, &notificationToCreate.PayloadOptions.InvitationAcceptedPayload.MemberAccess)
+
+		_, err := updateOrgRepo.createNotifComponent.TransactionBody(
+			operationOption,
+			notificationToCreate,
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return updatedMemberAccess, nil
 }
 
 func (updateOrgRepo *proposeUpdateMemberAccessRepository) RunTransaction(
 	input *model.InternalUpdateMemberAccess,
 ) (*model.MemberAccess, error) {
 	output, err := updateOrgRepo.mongoDBTransaction.RunTransaction(input)
-	return (output).(*model.MemberAccess), err
+	if err != nil {
+		return nil, err
+	}
+	return (output).(*model.MemberAccess), nil
 }
