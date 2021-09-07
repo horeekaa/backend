a1045b9b23477c495e98ff8505c582e837b88a59
stanley97dion <stanley97dion@gmail.com>
2021-09-07 21:46:49 +0700

    updateMouItem repos

---
+++ features/mouItems/data/repositories/updateMouItemComponent.go
@@ -0,0 +1,62 @@
+package mouitemdomainrepositories
+
+import (
+	"encoding/json"
+
+	mongodbcoretypes "github.com/horeekaa/backend/core/databaseClient/mongodb/types"
+	horeekaacoreexceptiontofailure "github.com/horeekaa/backend/core/errors/failures/exceptionToFailure"
+	databasemouitemdatasourceinterfaces "github.com/horeekaa/backend/features/mouItems/data/dataSources/databases/interfaces/sources"
+	mouitemdomainrepositoryinterfaces "github.com/horeekaa/backend/features/mouItems/domain/repositories"
+	mouitemdomainrepositoryutilityinterfaces "github.com/horeekaa/backend/features/mouItems/domain/repositories/utils"
+	"github.com/horeekaa/backend/model"
+)
+
+type updateMouItemDomainRepositories struct {
+	mouItemDataSource   databasemouitemdatasourceinterfaces.MouItemDataSource
+	agreedProductLoader mouitemdomainrepositoryutilityinterfaces.AgreedProductLoader
+}
+
+func NewUpdateMouItemDomainRepository(
+	mouItemDataSource databasemouitemdatasourceinterfaces.MouItemDataSource,
+	agreedProductLoader mouitemdomainrepositoryutilityinterfaces.AgreedProductLoader,
+) (mouitemdomainrepositoryinterfaces.UpdateMouItemTransactionComponent, error) {
+	return &updateMouItemDomainRepositories{
+		mouItemDataSource:   mouItemDataSource,
+		agreedProductLoader: agreedProductLoader,
+	}, nil
+}
+
+func (updateMouItemTrx *updateMouItemDomainRepositories) PreTransaction(
+	input *model.InternalUpdateMouItem,
+) (*model.InternalUpdateMouItem, error) {
+	return input, nil
+}
+
+func (updateMouItemTrx *updateMouItemDomainRepositories) TransactionBody(
+	session *mongodbcoretypes.OperationOptions,
+	updateMouItemInput *model.InternalUpdateMouItem,
+) (*model.MouItem, error) {
+	mouItemToUpdate := &model.DatabaseUpdateMouItem{}
+	jsonTemp, _ := json.Marshal(updateMouItemInput)
+	json.Unmarshal(jsonTemp, mouItemToUpdate)
+
+	updateMouItemTrx.agreedProductLoader.TransactionBody(
+		session,
+		mouItemToUpdate.Product,
+		mouItemToUpdate.AgreedProduct,
+	)
+
+	updatedVariant, err := updateMouItemTrx.mouItemDataSource.GetMongoDataSource().Update(
+		mouItemToUpdate.ID,
+		mouItemToUpdate,
+		session,
+	)
+	if err != nil {
+		return nil, horeekaacoreexceptiontofailure.ConvertException(
+			"/updateMouItem",
+			err,
+		)
+	}
+
+	return updatedVariant, nil
+}
