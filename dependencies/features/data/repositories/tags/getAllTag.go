b4be992c7986ee4b94a7fd5e3038cefd547a874c
stanley97dion <stanley97dion@gmail.com>
2021-09-17 22:17:55 +0700

    tag added query builder

--- dependencies/features/data/repositories/tags/getAllTag.go
+++ dependencies/features/data/repositories/tags/getAllTag.go
@@ -2,6 +2,7 @@ package tagdomainrepositorydependencies
 
 import (
 	"github.com/golobby/container/v2"
+	mongodbcorequerybuilderinterfaces "github.com/horeekaa/backend/core/databaseClient/mongodb/interfaces/queryBuilders"
 	databasetagdatasourceinterfaces "github.com/horeekaa/backend/features/tags/data/dataSources/databases/interfaces/sources"
 	tagdomainrepositories "github.com/horeekaa/backend/features/tags/data/repositories"
 	tagdomainrepositoryinterfaces "github.com/horeekaa/backend/features/tags/domain/repositories"
@@ -13,9 +14,11 @@ func (_ *GetAllTagDependency) Bind() {
 	container.Singleton(
 		func(
 			tagDataSource databasetagdatasourceinterfaces.TagDataSource,
+			mongoQueryBuilder mongodbcorequerybuilderinterfaces.MongoQueryBuilder,
 		) tagdomainrepositoryinterfaces.GetAllTagRepository {
 			getAllTagRepo, _ := tagdomainrepositories.NewGetAllTagRepository(
 				tagDataSource,
+				mongoQueryBuilder,
 			)
 			return getAllTagRepo
 		},
