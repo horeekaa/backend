702980c8bf1971197743350e187e43ab8ea6eaea
stanley97dion <stanley97dion@gmail.com>
2021-09-18 20:24:06 +0700

    organization added query builder

--- dependencies/features/data/repositories/organizations/getAllOrganization.go
+++ dependencies/features/data/repositories/organizations/getAllOrganization.go
@@ -2,6 +2,7 @@ package organizationdomainrepositorydependencies
 
 import (
 	"github.com/golobby/container/v2"
+	mongodbcorequerybuilderinterfaces "github.com/horeekaa/backend/core/databaseClient/mongodb/interfaces/queryBuilders"
 	databaseorganizationdatasourceinterfaces "github.com/horeekaa/backend/features/organizations/data/dataSources/databases/interfaces/sources"
 	organizationdomainrepositories "github.com/horeekaa/backend/features/organizations/data/repositories"
 	organizationdomainrepositoryinterfaces "github.com/horeekaa/backend/features/organizations/domain/repositories"
@@ -13,9 +14,11 @@ func (_ *GetAllOrganizationDependency) Bind() {
 	container.Singleton(
 		func(
 			organizationDataSource databaseorganizationdatasourceinterfaces.OrganizationDataSource,
+			mongoQueryBuilder mongodbcorequerybuilderinterfaces.MongoQueryBuilder,
 		) organizationdomainrepositoryinterfaces.GetAllOrganizationRepository {
 			getAllOrganizationRepo, _ := organizationdomainrepositories.NewGetAllOrganizationRepository(
 				organizationDataSource,
+				mongoQueryBuilder,
 			)
 			return getAllOrganizationRepo
 		},
