c8bec19d988fb1ccbe2d50edfc034d14eeef160b
stanley97dion <stanley97dion@gmail.com>
2021-09-18 20:58:56 +0700

    notification added query builder

--- dependencies/features/data/repositories/notifications/getAllNotification.go
+++ dependencies/features/data/repositories/notifications/getAllNotification.go
@@ -2,6 +2,7 @@ package notificationdomainrepositorydependencies
 
 import (
 	"github.com/golobby/container/v2"
+	mongodbcorequerybuilderinterfaces "github.com/horeekaa/backend/core/databaseClient/mongodb/interfaces/queryBuilders"
 	databasenotificationdatasourceinterfaces "github.com/horeekaa/backend/features/notifications/data/dataSources/databases/interfaces/sources"
 	notificationdomainrepositories "github.com/horeekaa/backend/features/notifications/data/repositories"
 	notificationdomainrepositoryinterfaces "github.com/horeekaa/backend/features/notifications/domain/repositories"
@@ -15,10 +16,12 @@ func (_ *GetAllNotificationDependency) Bind() {
 		func(
 			notificationDataSource databasenotificationdatasourceinterfaces.NotificationDataSource,
 			notifLocalizationBuilder notificationdomainrepositoryutilityinterfaces.NotificationLocalizationBuilder,
+			mongoQueryBuilder mongodbcorequerybuilderinterfaces.MongoQueryBuilder,
 		) notificationdomainrepositoryinterfaces.GetAllNotificationRepository {
 			getAllNotificationRepo, _ := notificationdomainrepositories.NewGetAllNotificationRepository(
 				notificationDataSource,
 				notifLocalizationBuilder,
+				mongoQueryBuilder,
 			)
 			return getAllNotificationRepo
 		},
