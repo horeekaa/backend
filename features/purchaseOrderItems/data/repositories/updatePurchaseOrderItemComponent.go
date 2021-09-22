76dc7fc18752608bce7f84727435dda04dc21908
stanley97dion <stanley97dion@gmail.com>
2021-09-22 22:01:29 +0700

    added subtotal calculation to po items repos

--- features/purchaseOrderItems/data/repositories/updatePurchaseOrderItemComponent.go
+++ features/purchaseOrderItems/data/repositories/updatePurchaseOrderItemComponent.go
@@ -9,6 +9,7 @@ import (
 	purchaseorderitemdomainrepositoryinterfaces "github.com/horeekaa/backend/features/purchaseOrderItems/domain/repositories"
 	purchaseorderitemdomainrepositoryutilityinterfaces "github.com/horeekaa/backend/features/purchaseOrderItems/domain/repositories/utils"
 	"github.com/horeekaa/backend/model"
+	"github.com/thoas/go-funk"
 )
 
 type updatePurchaseOrderItemTransactionComponent struct {
@@ -40,6 +41,17 @@ func (updatePurchaseOrderItemTrx *updatePurchaseOrderItemTransactionComponent) T
 	jsonTemp, _ := json.Marshal(updatepurchaseOrderItemInput)
 	json.Unmarshal(jsonTemp, purchaseOrderItemToUpdate)
 
+	existingPurchaseOrderItem, err := updatePurchaseOrderItemTrx.purchaseOrderItemDataSource.GetMongoDataSource().FindByID(
+		purchaseOrderItemToUpdate.ID,
+		session,
+	)
+	if err != nil {
+		return nil, horeekaacoreexceptiontofailure.ConvertException(
+			"/updatePurchaseOrderItem",
+			err,
+		)
+	}
+
 	if purchaseOrderItemToUpdate.ProductVariant != nil {
 		_, err := updatePurchaseOrderItemTrx.purchaseOrderItemLoader.TransactionBody(
 			session,
@@ -52,7 +64,31 @@ func (updatePurchaseOrderItemTrx *updatePurchaseOrderItemTransactionComponent) T
 				err,
 			)
 		}
+		purchaseOrderItemToUpdate.UnitPrice = &purchaseOrderItemToUpdate.ProductVariant.RetailPrice
+		if existingPurchaseOrderItem.MouItem != nil {
+			index := funk.IndexOf(
+				existingPurchaseOrderItem.MouItem.AgreedProduct.Variants,
+				func(pv *model.InternalAgreedProductVariantInput) bool {
+					return pv.ID == purchaseOrderItemToUpdate.ProductVariant.ID
+				},
+			)
+			if index > -1 {
+				purchaseOrderItemToUpdate.UnitPrice = &existingPurchaseOrderItem.MouItem.AgreedProduct.Variants[index].RetailPrice
+			}
+		}
 	}
+	unitPrice := existingPurchaseOrderItem.UnitPrice
+	if purchaseOrderItemToUpdate.UnitPrice != nil {
+		unitPrice = *purchaseOrderItemToUpdate.UnitPrice
+	}
+
+	quantity := existingPurchaseOrderItem.Quantity
+	if purchaseOrderItemToUpdate.Quantity != nil {
+		quantity = *purchaseOrderItemToUpdate.Quantity
+	}
+	subTotal := quantity * unitPrice
+
+	purchaseOrderItemToUpdate.SubTotal = &subTotal
 
 	updatedPurchaseOrderItem, err := updatePurchaseOrderItemTrx.purchaseOrderItemDataSource.GetMongoDataSource().Update(
 		map[string]interface{}{
