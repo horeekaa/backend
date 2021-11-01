94fef61ee425f624131c772e4d9c15f937a9b10f
stanley97dion <stanley97dion@gmail.com>
2021-11-01 19:15:06 +0700

    added access purchaseOrderReadOwned

--- features/purchaseOrders/domain/usecases/getAllPurchaseOrder.go
+++ features/purchaseOrders/domain/usecases/getAllPurchaseOrder.go
@@ -4,6 +4,8 @@ import (
 	horeekaacoreerror "github.com/horeekaa/backend/core/errors/errors"
 	horeekaacoreerrorenums "github.com/horeekaa/backend/core/errors/errors/enums"
 	horeekaacorefailuretoerror "github.com/horeekaa/backend/core/errors/errors/failureToError"
+	horeekaacorefailure "github.com/horeekaa/backend/core/errors/failures"
+	horeekaacorefailureenums "github.com/horeekaa/backend/core/errors/failures/enums"
 	accountdomainrepositoryinterfaces "github.com/horeekaa/backend/features/accounts/domain/repositories"
 	accountdomainrepositorytypes "github.com/horeekaa/backend/features/accounts/domain/repositories/types"
 	memberaccessdomainrepositoryinterfaces "github.com/horeekaa/backend/features/memberAccesses/domain/repositories"
@@ -13,6 +15,7 @@ import (
 	purchaseorderpresentationusecaseinterfaces "github.com/horeekaa/backend/features/purchaseOrders/presentation/usecases"
 	purchaseorderpresentationusecasetypes "github.com/horeekaa/backend/features/purchaseOrders/presentation/usecases/types"
 	"github.com/horeekaa/backend/model"
+	"github.com/thoas/go-funk"
 )
 
 type getAllPurchaseOrderUsecase struct {
@@ -81,13 +84,22 @@ func (getAllPOUcase *getAllPurchaseOrderUsecase) Execute(
 	}
 
 	memberAccessRefTypeOrgBased := model.MemberAccessRefTypeOrganizationsBased
-	_, err = getAllPOUcase.getAccountMemberAccessRepo.Execute(
+	memberAccess, err := getAllPOUcase.getAccountMemberAccessRepo.Execute(
 		memberaccessdomainrepositorytypes.GetAccountMemberAccessInput{
 			MemberAccessFilterFields: &model.InternalMemberAccessFilterFields{
 				Account:             &model.ObjectIDOnly{ID: &account.ID},
 				MemberAccessRefType: &memberAccessRefTypeOrgBased,
-				Access:              getAllPOUcase.getAllpurchaseOrderAccessIdentity,
+				Status: func(s model.MemberAccessStatus) *model.MemberAccessStatus {
+					return &s
+				}(model.MemberAccessStatusActive),
+				ProposalStatus: func(e model.EntityProposalStatus) *model.EntityProposalStatus {
+					return &e
+				}(model.EntityProposalStatusApproved),
+				InvitationAccepted: func(b bool) *bool {
+					return &b
+				}(true),
 			},
+			QueryMode: true,
 		},
 	)
 	if err != nil {
@@ -95,6 +107,26 @@ func (getAllPOUcase *getAllPurchaseOrderUsecase) Execute(
 			"/getAllPurchaseOrderUsecase",
 			err,
 		)
+	}
+	if accessible := funk.GetOrElse(
+		funk.Get(memberAccess, "Access.PurchaseOrderAccesses.PurchaseOrderReadAll"), false,
+	).(bool); !accessible {
+		if accessible := funk.GetOrElse(
+			funk.Get(memberAccess, "Access.PurchaseOrderAccesses.PurchaseOrderReadOwned"), false,
+		).(bool); accessible {
+			validatedInput.FilterFields.Organization = &model.OrganizationFilterFields{
+				ID: &memberAccess.Organization.ID,
+			}
+		} else {
+			return nil, horeekaacorefailuretoerror.ConvertFailure(
+				"/getAllPurchaseOrderUsecase",
+				horeekaacorefailure.NewFailureObject(
+					horeekaacorefailureenums.FeatureNotAccessibleByAccount,
+					"/getAllPurchaseOrderUsecase",
+					nil,
+				),
+			)
+		}
 	}
 
 	purchaseOrders, err := getAllPOUcase.getAllpurchaseOrderRepo.Execute(
