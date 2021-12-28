e29ce58cda0bad7c79d2cf70d0f99352173c4166
stanley97dion <stanley97dion@gmail.com>
2021-12-28 16:53:16 +0700

    Pick up and delivery apis (#58)
    * added timeSlot and arrivalDate field for delivery
    
    * add usecase, api, dependencies for POitem delivery
    
    * fix unintentional capital on pkg name
    
    * add usecase, api, dependencies for SOitem dependency
    
    * fix unintentional capital on SO pkg name
    
    * add photos for partner input on SO
    
    * photo file parsing from SORepo to SOItemRepo
    
    * added publicId for pickup/delivery
    
    * added flag for courier accepting task assignment
    
    * add noOfPackage field
    
    * add POItem after received descPhoto category
    
    * add field for photos after item received
    
    * added resolver for photos in delivery and pickup
    
    * fulfilled flag for POToSupply
    
    * added item distribution status to POToSupply
    
    * poItem add option to delete photo in array field
    
    * soItem add option to delete item in array field

--- features/supplyOrderItems/data/repositories/proposeUpdateSupplyOrderItemComponent.go
+++ features/supplyOrderItems/data/repositories/proposeUpdateSupplyOrderItemComponent.go
@@ -95,6 +95,19 @@ func (updateSupplyOrderItemTrx *proposeUpdateSupplyOrderItemTransactionComponent
 					)
 				}
 
+				if photoToUpdate.IsActive != nil {
+					if !*photoToUpdate.IsActive {
+						index := funk.IndexOf(
+							savedPhotos,
+							func(dp *model.DescriptivePhoto) bool {
+								return dp.ID == *photoToUpdate.ID
+							},
+						)
+						if index > -1 {
+							savedPhotos = append(savedPhotos[:index], savedPhotos[index+1:]...)
+						}
+					}
+				}
 				continue
 			}
 			photoToCreate := &model.InternalCreateDescriptivePhoto{}
@@ -164,6 +177,19 @@ func (updateSupplyOrderItemTrx *proposeUpdateSupplyOrderItemTransactionComponent
 					)
 				}
 
+				if photoToUpdate.IsActive != nil {
+					if !*photoToUpdate.IsActive {
+						index := funk.IndexOf(
+							savedPhotos,
+							func(dp *model.DescriptivePhoto) bool {
+								return dp.ID == *photoToUpdate.ID
+							},
+						)
+						if index > -1 {
+							savedPhotos = append(savedPhotos[:index], savedPhotos[index+1:]...)
+						}
+					}
+				}
 				continue
 			}
 			photoToCreate := &model.InternalCreateDescriptivePhoto{}
