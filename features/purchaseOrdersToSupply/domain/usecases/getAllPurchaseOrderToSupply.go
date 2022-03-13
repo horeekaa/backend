9be2ff94f09f02a019f4f565d8e705637b00973a
stanley97dion <stanley97dion@gmail.com>
2022-03-13 22:29:13 +0700

    poToSupply merge access with Supply Order

--- features/purchaseOrdersToSupply/domain/usecases/getAllPurchaseOrderToSupply.go
+++ features/purchaseOrdersToSupply/domain/usecases/getAllPurchaseOrderToSupply.go
@@ -4,6 +4,8 @@ import (
 	horeekaacoreerror "github.com/horeekaa/backend/core/errors/errors"
 	horeekaacoreerrorenums "github.com/horeekaa/backend/core/errors/errors/enums"
 	horeekaacorefailuretoerror "github.com/horeekaa/backend/core/errors/errors/failureToError"
+	horeekaacorefailure "github.com/horeekaa/backend/core/errors/failures"
+	horeekaacorefailureenums "github.com/horeekaa/backend/core/errors/failures/enums"
 	accountdomainrepositoryinterfaces "github.com/horeekaa/backend/features/accounts/domain/repositories"
 	accountdomainrepositorytypes "github.com/horeekaa/backend/features/accounts/domain/repositories/types"
 	memberaccessdomainrepositoryinterfaces "github.com/horeekaa/backend/features/memberAccesses/domain/repositories"
@@ -13,6 +15,7 @@ import (
 	purchaseordertosupplypresentationusecaseinterfaces "github.com/horeekaa/backend/features/purchaseOrdersToSupply/presentation/usecases"
 	purchaseordertosupplypresentationusecasetypes "github.com/horeekaa/backend/features/purchaseOrdersToSupply/presentation/usecases/types"
 	"github.com/horeekaa/backend/model"
+	"github.com/thoas/go-funk"
 )
 
 type getAllPurchaseOrderToSupplyUsecase struct {
@@ -33,8 +36,8 @@ func NewGetAllPurchaseOrderToSupplyUsecase(
 		getAccountMemberAccessRepo,
 		getAllPurchaseOrderToSupplyRepo,
 		&model.MemberAccessRefOptionsInput{
-			PurchaseOrderAccesses: &model.PurchaseOrderAccessesInput{
-				PurchaseOrderReadAll: func(b bool) *bool { return &b }(true),
+			SupplyOrderAccesses: &model.SupplyOrderAccessesInput{
+				SupplyOrderReadAll: func(b bool) *bool { return &b }(true),
 			},
 		},
 		"GetAllPurchaseOrderToSupplyUsecase",
@@ -81,13 +84,22 @@ func (getAllPOToSupplyUcase *getAllPurchaseOrderToSupplyUsecase) Execute(
 	}
 
 	memberAccessRefTypeOrgBased := model.MemberAccessRefTypeOrganizationsBased
-	_, err = getAllPOToSupplyUcase.getAccountMemberAccessRepo.Execute(
+	memberAccess, err := getAllPOToSupplyUcase.getAccountMemberAccessRepo.Execute(
 		memberaccessdomainrepositorytypes.GetAccountMemberAccessInput{
 			MemberAccessFilterFields: &model.InternalMemberAccessFilterFields{
 				Account:             &model.ObjectIDOnly{ID: &account.ID},
 				MemberAccessRefType: &memberAccessRefTypeOrgBased,
-				Access:              getAllPOToSupplyUcase.getAllPurchaseOrderAccessIdentity,
+				Status: func(s model.MemberAccessStatus) *model.MemberAccessStatus {
+					return &s
+				}(model.MemberAccessStatusActive),
+				ProposalStatus: func(e model.EntityProposalStatus) *model.EntityProposalStatus {
+					return &e
+				}(model.EntityProposalStatusApproved),
+				InvitationAccepted: func(b bool) *bool {
+					return &b
+				}(true),
 			},
+			QueryMode: true,
 		},
 	)
 	if err != nil {
@@ -95,6 +107,23 @@ func (getAllPOToSupplyUcase *getAllPurchaseOrderToSupplyUsecase) Execute(
 			getAllPOToSupplyUcase.pathIdentity,
 			err,
 		)
+	}
+	if accessible := funk.GetOrElse(
+		funk.Get(memberAccess, "Access.SupplyOrderAccesses.SupplyOrderReadAll"), false,
+	).(bool); !accessible {
+		if accessible := funk.GetOrElse(
+			funk.Get(memberAccess, "Access.SupplyOrderAccesses.SupplyOrderReadOwned"), false,
+		).(bool); accessible {
+		} else {
+			return nil, horeekaacorefailuretoerror.ConvertFailure(
+				getAllPOToSupplyUcase.pathIdentity,
+				horeekaacorefailure.NewFailureObject(
+					horeekaacorefailureenums.FeatureNotAccessibleByAccount,
+					getAllPOToSupplyUcase.pathIdentity,
+					nil,
+				),
+			)
+		}
 	}
 
 	purchaseOrdersToSupply, err := getAllPOToSupplyUcase.getAllPurchaseOrderToSupplyRepo.Execute(
