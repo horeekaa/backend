5d11ae82f8892b39b9d864a32f15e926ac36f0fd
stanley97dion <stanley97dion@gmail.com>
2022-03-13 22:20:25 +0700

    gql route moved to separate file

--- server.go
+++ server.go
@@ -5,18 +5,14 @@ import (
 	"net/http"
 	"os"
 
-	authenticationmiddlewares "github.com/horeekaa/backend/http/middlewares/authentication"
+	graphqlroutes "github.com/horeekaa/backend/http/routes/graphql"
 	scheduledjobroutes "github.com/horeekaa/backend/http/routes/scheduledJob"
+
 	"github.com/joho/godotenv"
 
 	masterdependencies "github.com/horeekaa/backend/dependencies"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi"
-
-	"github.com/horeekaa/backend/graph/generated"
-	graphresolver "github.com/horeekaa/backend/graph/resolver"
 )
 
 const defaultPort = "8080"
@@ -35,17 +31,9 @@ func main() {
 	}
 	router := chi.NewRouter()
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graphresolver.Resolver{}}))
-
 	router.Route("/api", func(r chi.Router) {
 		r.Route("/v1", func(r chi.Router) {
-			r.Route("/graphql", func(r chi.Router) {
-				r.Get("/", playground.Handler("GraphQL playground", "/api/v1/graphql/query"))
-				r.Route("/query", func(r chi.Router) {
-					r.Use(authenticationmiddlewares.AuthGateMiddleware)
-					r.Handle("/", srv)
-				})
-			})
+			r.Route("/graphql", graphqlroutes.Route)
 		})
 	})
 
