fbe622a17a6937fba10a205b44958b93d8761f7f
stanley97dion <stanley97dion@gmail.com>
2022-03-17 16:35:00 +0700

    addressRegionGroup proposed resolver

---
+++ graph/resolver/addressRegionGroupProposed.resolvers.go
@@ -0,0 +1,64 @@
+package graphresolver
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+
+	"github.com/golobby/container/v2"
+	accountpresentationusecaseinterfaces "github.com/horeekaa/backend/features/accounts/presentation/usecases"
+	accountpresentationusecasetypes "github.com/horeekaa/backend/features/accounts/presentation/usecases/types"
+	loggingpresentationusecaseinterfaces "github.com/horeekaa/backend/features/loggings/presentation/usecases"
+	"github.com/horeekaa/backend/graph/generated"
+	"github.com/horeekaa/backend/model"
+)
+
+func (r *addressRegionGroupProposedResolver) SubmittingAccount(ctx context.Context, obj *model.AddressRegionGroupProposed) (*model.Account, error) {
+	var getAccountUsecase accountpresentationusecaseinterfaces.GetAccountUsecase
+	container.Make(&getAccountUsecase)
+	return getAccountUsecase.Execute(
+		accountpresentationusecasetypes.GetAccountInput{
+			FilterFields: &model.AccountFilterFields{
+				ID: &obj.SubmittingAccount.ID,
+			},
+		},
+	)
+}
+
+func (r *addressRegionGroupProposedResolver) RecentApprovingAccount(ctx context.Context, obj *model.AddressRegionGroupProposed) (*model.Account, error) {
+	var getAccountUsecase accountpresentationusecaseinterfaces.GetAccountUsecase
+	container.Make(&getAccountUsecase)
+
+	var filterFields *model.AccountFilterFields
+	if obj.RecentApprovingAccount != nil {
+		filterFields = &model.AccountFilterFields{}
+		filterFields.ID = &obj.RecentApprovingAccount.ID
+	}
+	return getAccountUsecase.Execute(
+		accountpresentationusecasetypes.GetAccountInput{
+			FilterFields: filterFields,
+		},
+	)
+}
+
+func (r *addressRegionGroupProposedResolver) RecentLog(ctx context.Context, obj *model.AddressRegionGroupProposed) (*model.Logging, error) {
+	var getLoggingUsecase loggingpresentationusecaseinterfaces.GetLoggingUsecase
+	container.Make(&getLoggingUsecase)
+
+	var filterFields *model.LoggingFilterFields
+	if obj.RecentLog != nil {
+		filterFields = &model.LoggingFilterFields{}
+		filterFields.ID = &obj.RecentLog.ID
+	}
+	return getLoggingUsecase.Execute(
+		filterFields,
+	)
+}
+
+// AddressRegionGroupProposed returns generated.AddressRegionGroupProposedResolver implementation.
+func (r *Resolver) AddressRegionGroupProposed() generated.AddressRegionGroupProposedResolver {
+	return &addressRegionGroupProposedResolver{r}
+}
+
+type addressRegionGroupProposedResolver struct{ *Resolver }
