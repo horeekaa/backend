3f89bdadd9ba6098daa171b875402a0c0aabe32d
stanley97dion <stanley97dion@gmail.com>
2022-03-17 17:08:28 +0700

    missing container import

--- graph/resolver/addressRegionGroupProposed.resolvers.go
+++ graph/resolver/addressRegionGroupProposed.resolvers.go
@@ -6,7 +6,7 @@ package graphresolver
 import (
 	"context"
 
-	"github.com/golobby/container/v2"
+	container "github.com/golobby/container/v2"
 	accountpresentationusecaseinterfaces "github.com/horeekaa/backend/features/accounts/presentation/usecases"
 	accountpresentationusecasetypes "github.com/horeekaa/backend/features/accounts/presentation/usecases/types"
 	loggingpresentationusecaseinterfaces "github.com/horeekaa/backend/features/loggings/presentation/usecases"
