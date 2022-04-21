1c3a403a5d4be2209e0e49d0f055ce0718fb9367
stanley97dion <stanley97dion@gmail.com>
2022-04-21 17:32:06 +0700

    photos resolver on po so return

---
+++ graph/resolver/purchaseOrderItemReturn.resolvers.go
@@ -0,0 +1,42 @@
+package graphresolver
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+
+	container "github.com/golobby/container/v2"
+	descriptivephotopresentationusecaseinterfaces "github.com/horeekaa/backend/features/descriptivePhotos/presentation/usecases"
+	"github.com/horeekaa/backend/graph/generated"
+	"github.com/horeekaa/backend/model"
+)
+
+func (r *purchaseOrderItemReturnResolver) Photos(ctx context.Context, obj *model.PurchaseOrderItemReturn) ([]*model.DescriptivePhoto, error) {
+	var getDescriptivePhotoUsecase descriptivephotopresentationusecaseinterfaces.GetDescriptivePhotoUsecase
+	container.Make(&getDescriptivePhotoUsecase)
+
+	descriptivePhotos := []*model.DescriptivePhoto{}
+	if obj.Photos != nil {
+		for _, photo := range obj.Photos {
+			descriptivePhoto, err := getDescriptivePhotoUsecase.Execute(
+				&model.DescriptivePhotoFilterFields{
+					ID: &photo.ID,
+				},
+			)
+			if err != nil {
+				return nil, err
+			}
+
+			descriptivePhotos = append(descriptivePhotos, descriptivePhoto)
+		}
+	}
+	return descriptivePhotos, nil
+}
+
+// PurchaseOrderItemReturn returns generated.PurchaseOrderItemReturnResolver implementation.
+func (r *Resolver) PurchaseOrderItemReturn() generated.PurchaseOrderItemReturnResolver {
+	return &purchaseOrderItemReturnResolver{r}
+}
+
+type purchaseOrderItemReturnResolver struct{ *Resolver }
